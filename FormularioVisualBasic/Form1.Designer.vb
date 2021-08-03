6db6237694a7716be20aea147e08a36261c33cb2
Programador Novato <info@programadornovato.com>
2021-08-03 04:41:09 -0500

    Ejercicio 01.- Propiedades de las ventanas en Visual Basic [02]⛓️ Video: https://youtu.be/hlchqEFmlXE

--- FormularioVisualBasic/Form1.Designer.vb
+++ FormularioVisualBasic/Form1.Designer.vb
@@ -23,28 +23,52 @@ Partial Class Form1
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
         Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
         Me.SuspendLayout()
         '
         'Button1
         '
-        Me.Button1.Location = New System.Drawing.Point(399, 307)
+        Me.Button1.Location = New System.Drawing.Point(15, 26)
+        Me.Button1.Margin = New System.Windows.Forms.Padding(6, 5, 6, 5)
         Me.Button1.Name = "Button1"
-        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.Size = New System.Drawing.Size(139, 38)
         Me.Button1.TabIndex = 0
         Me.Button1.Text = "Button1"
         Me.Button1.UseVisualStyleBackColor = True
         '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(188, 26)
+        Me.Button2.Margin = New System.Windows.Forms.Padding(6, 5, 6, 5)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(139, 38)
+        Me.Button2.TabIndex = 1
+        Me.Button2.Text = "Button2"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
         'Form1
         '
-        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
+        Me.AcceptButton = Me.Button2
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(13.0!, 25.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(539, 430)
+        Me.BackColor = System.Drawing.SystemColors.Control
+        Me.BackgroundImage = Global.FormularioVisualBasic.My.Resources.Resources.logo
+        Me.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center
+        Me.ClientSize = New System.Drawing.Size(427, 338)
+        Me.Controls.Add(Me.Button2)
         Me.Controls.Add(Me.Button1)
+        Me.Font = New System.Drawing.Font("Tahoma", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.Location = New System.Drawing.Point(100, 100)
+        Me.Margin = New System.Windows.Forms.Padding(6, 5, 6, 5)
+        Me.MaximumSize = New System.Drawing.Size(500, 500)
+        Me.MinimumSize = New System.Drawing.Size(300, 300)
         Me.Name = "Form1"
-        Me.Text = "Form1"
+        Me.Opacity = 0.5R
+        Me.Text = "Formulario 1"
         Me.ResumeLayout(False)
 
     End Sub
 
     Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
 End Class
