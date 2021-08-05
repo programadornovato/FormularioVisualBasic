515a8f63e22f32102055b13547b81ea19bcd7ac7
Programador Novato <info@programadornovato.com>
2021-08-05 02:37:07 -0500

    Eventos y progressBar en Visual Basic [07]⛓️ Video: https://youtu.be/f9zCCVx_Qys

--- FormularioVisualBasic/Ventana2.Designer.vb
+++ FormularioVisualBasic/Ventana2.Designer.vb
@@ -1,9 +1,9 @@
-﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
 Partial Class Ventana2
     Inherits System.Windows.Forms.Form
 
     'Form reemplaza a Dispose para limpiar la lista de componentes.
-    <System.Diagnostics.DebuggerNonUserCode()> _
+    <System.Diagnostics.DebuggerNonUserCode()>
     Protected Overrides Sub Dispose(ByVal disposing As Boolean)
         Try
             If disposing AndAlso components IsNot Nothing Then
@@ -20,109 +20,58 @@ Partial Class Ventana2
     'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
     'Se puede modificar usando el Diseñador de Windows Forms.  
     'No lo modifique con el editor de código.
-    <System.Diagnostics.DebuggerStepThrough()> _
+    <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
-        Me.btnRegresar = New System.Windows.Forms.Button()
-        Me.btnPagar = New System.Windows.Forms.Button()
-        Me.lblResultado = New System.Windows.Forms.Label()
-        Me.cbProductos = New System.Windows.Forms.ComboBox()
-        Me.lbPasarelas = New System.Windows.Forms.ListBox()
-        Me.imgProductos = New System.Windows.Forms.PictureBox()
-        Me.imgPasarelas = New System.Windows.Forms.PictureBox()
-        CType(Me.imgProductos, System.ComponentModel.ISupportInitialize).BeginInit()
-        CType(Me.imgPasarelas, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.imgLogo = New System.Windows.Forms.PictureBox()
+        Me.pbCarga = New System.Windows.Forms.ProgressBar()
+        Me.lblPos = New System.Windows.Forms.Label()
+        CType(Me.imgLogo, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
-        'btnRegresar
+        'imgLogo
         '
-        Me.btnRegresar.Location = New System.Drawing.Point(34, 322)
-        Me.btnRegresar.Name = "btnRegresar"
-        Me.btnRegresar.Size = New System.Drawing.Size(130, 44)
-        Me.btnRegresar.TabIndex = 4
-        Me.btnRegresar.Text = "Regresar"
-        Me.btnRegresar.UseVisualStyleBackColor = True
+        Me.imgLogo.Image = Global.FormularioVisualBasic.My.Resources.Resources.logo
+        Me.imgLogo.Location = New System.Drawing.Point(291, 106)
+        Me.imgLogo.Name = "imgLogo"
+        Me.imgLogo.Size = New System.Drawing.Size(188, 173)
+        Me.imgLogo.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.imgLogo.TabIndex = 0
+        Me.imgLogo.TabStop = False
         '
-        'btnPagar
+        'pbCarga
         '
-        Me.btnPagar.Location = New System.Drawing.Point(371, 322)
-        Me.btnPagar.Name = "btnPagar"
-        Me.btnPagar.Size = New System.Drawing.Size(130, 44)
-        Me.btnPagar.TabIndex = 4
-        Me.btnPagar.Text = "Pagar"
-        Me.btnPagar.UseVisualStyleBackColor = True
+        Me.pbCarga.Location = New System.Drawing.Point(224, 334)
+        Me.pbCarga.Name = "pbCarga"
+        Me.pbCarga.Size = New System.Drawing.Size(296, 40)
+        Me.pbCarga.TabIndex = 1
         '
-        'lblResultado
+        'lblPos
         '
-        Me.lblResultado.AutoSize = True
-        Me.lblResultado.Location = New System.Drawing.Point(34, 416)
-        Me.lblResultado.Name = "lblResultado"
-        Me.lblResultado.Size = New System.Drawing.Size(142, 25)
-        Me.lblResultado.TabIndex = 5
-        Me.lblResultado.Text = "lblResultado"
-        '
-        'cbProductos
-        '
-        Me.cbProductos.FormattingEnabled = True
-        Me.cbProductos.Items.AddRange(New Object() {"Camisas 500", "Tenis 1000"})
-        Me.cbProductos.Location = New System.Drawing.Point(34, 47)
-        Me.cbProductos.Name = "cbProductos"
-        Me.cbProductos.Size = New System.Drawing.Size(162, 33)
-        Me.cbProductos.TabIndex = 6
-        '
-        'lbPasarelas
-        '
-        Me.lbPasarelas.FormattingEnabled = True
-        Me.lbPasarelas.ItemHeight = 25
-        Me.lbPasarelas.Items.AddRange(New Object() {"Paypal", "Tarjeta"})
-        Me.lbPasarelas.Location = New System.Drawing.Point(371, 47)
-        Me.lbPasarelas.Name = "lbPasarelas"
-        Me.lbPasarelas.Size = New System.Drawing.Size(120, 79)
-        Me.lbPasarelas.TabIndex = 7
-        '
-        'imgProductos
-        '
-        Me.imgProductos.Location = New System.Drawing.Point(34, 164)
-        Me.imgProductos.Name = "imgProductos"
-        Me.imgProductos.Size = New System.Drawing.Size(156, 114)
-        Me.imgProductos.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
-        Me.imgProductos.TabIndex = 8
-        Me.imgProductos.TabStop = False
-        '
-        'imgPasarelas
-        '
-        Me.imgPasarelas.Location = New System.Drawing.Point(371, 164)
-        Me.imgPasarelas.Name = "imgPasarelas"
-        Me.imgPasarelas.Size = New System.Drawing.Size(156, 114)
-        Me.imgPasarelas.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
-        Me.imgPasarelas.TabIndex = 8
-        Me.imgPasarelas.TabStop = False
+        Me.lblPos.AutoSize = True
+        Me.lblPos.Location = New System.Drawing.Point(224, 392)
+        Me.lblPos.Name = "lblPos"
+        Me.lblPos.Size = New System.Drawing.Size(81, 25)
+        Me.lblPos.TabIndex = 2
+        Me.lblPos.Text = "Label1"
         '
         'Ventana2
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(13.0!, 25.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(800, 450)
-        Me.Controls.Add(Me.imgPasarelas)
-        Me.Controls.Add(Me.imgProductos)
-        Me.Controls.Add(Me.lbPasarelas)
-        Me.Controls.Add(Me.cbProductos)
-        Me.Controls.Add(Me.lblResultado)
-        Me.Controls.Add(Me.btnPagar)
-        Me.Controls.Add(Me.btnRegresar)
+        Me.Controls.Add(Me.lblPos)
+        Me.Controls.Add(Me.pbCarga)
+        Me.Controls.Add(Me.imgLogo)
         Me.Font = New System.Drawing.Font("Tahoma", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
         Me.Name = "Ventana2"
         Me.Text = "Ventana2"
-        CType(Me.imgProductos, System.ComponentModel.ISupportInitialize).EndInit()
-        CType(Me.imgPasarelas, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.imgLogo, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
     End Sub
-    Friend WithEvents btnRegresar As Button
-    Friend WithEvents btnPagar As Button
-    Friend WithEvents lblResultado As Label
-    Friend WithEvents cbProductos As ComboBox
-    Friend WithEvents lbPasarelas As ListBox
-    Friend WithEvents imgProductos As PictureBox
-    Friend WithEvents imgPasarelas As PictureBox
+
+    Friend WithEvents imgLogo As PictureBox
+    Friend WithEvents pbCarga As ProgressBar
+    Friend WithEvents lblPos As Label
 End Class
--- FormularioVisualBasic/Ventana2.vb
+++ FormularioVisualBasic/Ventana2.vb
@@ -1,48 +1,22 @@
 ﻿Public Class Ventana2
+    Dim con = 0
+    Private Sub imgLogo_Click(sender As Object, e As EventArgs) Handles imgLogo.Click
+        carga()
+    End Sub
+
     Private Sub Ventana2_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-        lblResultado.Text = ""
+        pbCarga.Minimum = 0
+        pbCarga.Maximum = 100
     End Sub
 
-    Private Sub btnPagar_Click(sender As Object, e As EventArgs) Handles btnPagar.Click
-        Dim total = 0
-        Dim pasarela = ""
-        Dim conProd = 0
-        Dim conPasarelas = 0
-        If cbProductos.Text = "Camisas 500" Then
-            total = total + 500
-            conProd = conProd + 1
-        End If
-        If cbProductos.Text = "Tenis 1000" Then
-            total = total + 1000
-            conProd = conProd + 1
-        End If
-        If lbPasarelas.Text = "Paypal" Then
-            pasarela = "Pago con paypal "
-            conPasarelas = conPasarelas + 1
-        End If
-        If lbPasarelas.Text = "Tarjeta" Then
-            pasarela = "Pago con tarjeta "
-            conPasarelas = conPasarelas + 1
-        End If
-        If conProd > 0 And conPasarelas > 0 Then
-            lblResultado.Text = pasarela & total
-        Else
-            lblResultado.Text = "Humano debes seleccionar un producto y una pasarela"
-        End If
-
+    Private Sub imgLogo_MouseMove(sender As Object, e As MouseEventArgs) Handles imgLogo.MouseMove
+        carga()
+        lblPos.Text = "X=" & e.X & " Y=" & e.Y
     End Sub
-
-    Private Sub btnRegresar_Click(sender As Object, e As EventArgs) Handles btnRegresar.Click
-        Dim f = New Form1()
-        f.Show()
-        Me.Hide()
-    End Sub
-
-    Private Sub cbProductos_SelectedIndexChanged(sender As Object, e As EventArgs) Handles cbProductos.SelectedIndexChanged
-        imgProductos.ImageLocation = "C:/Users/eugenio/Pictures/VB/" & cbProductos.Text & ".png"
-    End Sub
-
-    Private Sub lbPasarelas_SelectedIndexChanged(sender As Object, e As EventArgs) Handles lbPasarelas.SelectedIndexChanged
-        imgPasarelas.ImageLocation = "C:/Users/eugenio/Pictures/VB/" & lbPasarelas.Text & ".png"
+    Sub carga()
+        If con < 100 Then
+            con = con + 1
+            pbCarga.Value = con
+        End If
     End Sub
 End Class
