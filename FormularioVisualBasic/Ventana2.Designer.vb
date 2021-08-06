9f8b0d822e6d2196f841f13e7efc0ccf5bf46344
Programador Novato <info@programadornovato.com>
2021-08-05 20:56:32 -0500

    DatagridView (tablas) en Visual Basic [08]⛓️ Video: https://youtu.be/FMqcE4LMB9U

--- FormularioVisualBasic/Ventana2.Designer.vb
+++ FormularioVisualBasic/Ventana2.Designer.vb
@@ -22,56 +22,157 @@ Partial Class Ventana2
     'No lo modifique con el editor de código.
     <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
-        Me.imgLogo = New System.Windows.Forms.PictureBox()
-        Me.pbCarga = New System.Windows.Forms.ProgressBar()
-        Me.lblPos = New System.Windows.Forms.Label()
-        CType(Me.imgLogo, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.txtNombre = New System.Windows.Forms.TextBox()
+        Me.txtApellido = New System.Windows.Forms.TextBox()
+        Me.txtEdad = New System.Windows.Forms.TextBox()
+        Me.dgPersonas = New System.Windows.Forms.DataGridView()
+        Me.colNombre = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.colApellido = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.colEdad = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.btnAgregar = New System.Windows.Forms.Button()
+        Me.btnBorrar = New System.Windows.Forms.Button()
+        Me.lblDatos = New System.Windows.Forms.Label()
+        CType(Me.dgPersonas, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
-        'imgLogo
+        'Label1
         '
-        Me.imgLogo.Image = Global.FormularioVisualBasic.My.Resources.Resources.logo
-        Me.imgLogo.Location = New System.Drawing.Point(291, 106)
-        Me.imgLogo.Name = "imgLogo"
-        Me.imgLogo.Size = New System.Drawing.Size(188, 173)
-        Me.imgLogo.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
-        Me.imgLogo.TabIndex = 0
-        Me.imgLogo.TabStop = False
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(39, 31)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(95, 25)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Nombre"
         '
-        'pbCarga
+        'Label2
         '
-        Me.pbCarga.Location = New System.Drawing.Point(224, 334)
-        Me.pbCarga.Name = "pbCarga"
-        Me.pbCarga.Size = New System.Drawing.Size(296, 40)
-        Me.pbCarga.TabIndex = 1
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(39, 70)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(95, 25)
+        Me.Label2.TabIndex = 0
+        Me.Label2.Text = "Apellido"
         '
-        'lblPos
+        'Label3
         '
-        Me.lblPos.AutoSize = True
-        Me.lblPos.Location = New System.Drawing.Point(224, 392)
-        Me.lblPos.Name = "lblPos"
-        Me.lblPos.Size = New System.Drawing.Size(81, 25)
-        Me.lblPos.TabIndex = 2
-        Me.lblPos.Text = "Label1"
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(39, 107)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(64, 25)
+        Me.Label3.TabIndex = 0
+        Me.Label3.Text = "Edad"
+        '
+        'txtNombre
+        '
+        Me.txtNombre.Location = New System.Drawing.Point(187, 32)
+        Me.txtNombre.Name = "txtNombre"
+        Me.txtNombre.Size = New System.Drawing.Size(227, 33)
+        Me.txtNombre.TabIndex = 0
+        '
+        'txtApellido
+        '
+        Me.txtApellido.Location = New System.Drawing.Point(187, 71)
+        Me.txtApellido.Name = "txtApellido"
+        Me.txtApellido.Size = New System.Drawing.Size(227, 33)
+        Me.txtApellido.TabIndex = 1
+        '
+        'txtEdad
+        '
+        Me.txtEdad.Location = New System.Drawing.Point(187, 110)
+        Me.txtEdad.Name = "txtEdad"
+        Me.txtEdad.Size = New System.Drawing.Size(227, 33)
+        Me.txtEdad.TabIndex = 2
+        '
+        'dgPersonas
+        '
+        Me.dgPersonas.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.dgPersonas.Columns.AddRange(New System.Windows.Forms.DataGridViewColumn() {Me.colNombre, Me.colApellido, Me.colEdad})
+        Me.dgPersonas.Location = New System.Drawing.Point(39, 194)
+        Me.dgPersonas.Name = "dgPersonas"
+        Me.dgPersonas.RowTemplate.Height = 25
+        Me.dgPersonas.Size = New System.Drawing.Size(375, 150)
+        Me.dgPersonas.TabIndex = 5
+        '
+        'colNombre
+        '
+        Me.colNombre.HeaderText = "Nombre"
+        Me.colNombre.Name = "colNombre"
+        '
+        'colApellido
+        '
+        Me.colApellido.HeaderText = "Apellido"
+        Me.colApellido.Name = "colApellido"
+        '
+        'colEdad
+        '
+        Me.colEdad.HeaderText = "Edad"
+        Me.colEdad.Name = "colEdad"
+        '
+        'btnAgregar
+        '
+        Me.btnAgregar.Location = New System.Drawing.Point(519, 32)
+        Me.btnAgregar.Name = "btnAgregar"
+        Me.btnAgregar.Size = New System.Drawing.Size(127, 44)
+        Me.btnAgregar.TabIndex = 3
+        Me.btnAgregar.Text = "Agregar"
+        Me.btnAgregar.UseVisualStyleBackColor = True
+        '
+        'btnBorrar
+        '
+        Me.btnBorrar.Location = New System.Drawing.Point(519, 88)
+        Me.btnBorrar.Name = "btnBorrar"
+        Me.btnBorrar.Size = New System.Drawing.Size(127, 44)
+        Me.btnBorrar.TabIndex = 4
+        Me.btnBorrar.Text = "Borrar"
+        Me.btnBorrar.UseVisualStyleBackColor = True
+        '
+        'lblDatos
+        '
+        Me.lblDatos.AutoSize = True
+        Me.lblDatos.Location = New System.Drawing.Point(39, 381)
+        Me.lblDatos.Name = "lblDatos"
+        Me.lblDatos.Size = New System.Drawing.Size(99, 25)
+        Me.lblDatos.TabIndex = 4
+        Me.lblDatos.Text = "lblDatos"
         '
         'Ventana2
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(13.0!, 25.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(800, 450)
-        Me.Controls.Add(Me.lblPos)
-        Me.Controls.Add(Me.pbCarga)
-        Me.Controls.Add(Me.imgLogo)
+        Me.Controls.Add(Me.lblDatos)
+        Me.Controls.Add(Me.btnBorrar)
+        Me.Controls.Add(Me.btnAgregar)
+        Me.Controls.Add(Me.dgPersonas)
+        Me.Controls.Add(Me.txtEdad)
+        Me.Controls.Add(Me.txtApellido)
+        Me.Controls.Add(Me.txtNombre)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
         Me.Font = New System.Drawing.Font("Tahoma", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
         Me.Name = "Ventana2"
         Me.Text = "Ventana2"
-        CType(Me.imgLogo, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.dgPersonas, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
     End Sub
 
-    Friend WithEvents imgLogo As PictureBox
-    Friend WithEvents pbCarga As ProgressBar
-    Friend WithEvents lblPos As Label
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents txtNombre As TextBox
+    Friend WithEvents txtApellido As TextBox
+    Friend WithEvents txtEdad As TextBox
+    Friend WithEvents dgPersonas As DataGridView
+    Friend WithEvents colNombre As DataGridViewTextBoxColumn
+    Friend WithEvents colApellido As DataGridViewTextBoxColumn
+    Friend WithEvents colEdad As DataGridViewTextBoxColumn
+    Friend WithEvents btnAgregar As Button
+    Friend WithEvents btnBorrar As Button
+    Friend WithEvents lblDatos As Label
 End Class
--- FormularioVisualBasic/Ventana2.vb
+++ FormularioVisualBasic/Ventana2.vb
@@ -1,22 +1,34 @@
 ﻿Public Class Ventana2
-    Dim con = 0
-    Private Sub imgLogo_Click(sender As Object, e As EventArgs) Handles imgLogo.Click
-        carga()
-    End Sub
-
+    Dim registroSel = 0
     Private Sub Ventana2_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-        pbCarga.Minimum = 0
-        pbCarga.Maximum = 100
+        lblDatos.Text = ""
     End Sub
 
-    Private Sub imgLogo_MouseMove(sender As Object, e As MouseEventArgs) Handles imgLogo.MouseMove
-        carga()
-        lblPos.Text = "X=" & e.X & " Y=" & e.Y
+    Private Sub btnAgregar_Click(sender As Object, e As EventArgs) Handles btnAgregar.Click
+        Dim n = dgPersonas.Rows.Add()
+        dgPersonas.Rows(n).Cells(0).Value = txtNombre.Text
+        dgPersonas.Rows(n).Cells(1).Value = txtApellido.Text
+        dgPersonas.Rows(n).Cells(2).Value = txtEdad.Text
+        txtNombre.Text = ""
+        txtApellido.Text = ""
+        txtEdad.Text = ""
     End Sub
-    Sub carga()
-        If con < 100 Then
-            con = con + 1
-            pbCarga.Value = con
+
+    Private Sub dgPersonas_CellClick(sender As Object, e As DataGridViewCellEventArgs) Handles dgPersonas.CellClick
+        Dim n = dgPersonas.Rows.Count - 1
+        registroSel = e.RowIndex
+        If registroSel <> -1 And registroSel < n Then
+            Dim nombre = dgPersonas.Rows(registroSel).Cells(0).Value.ToString
+            Dim apellido = dgPersonas.Rows(registroSel).Cells(1).Value.ToString
+            Dim edad = dgPersonas.Rows(registroSel).Cells(2).Value.ToString
+            lblDatos.Text = nombre & " " & apellido & " " & edad
+        End If
+    End Sub
+
+    Private Sub btnBorrar_Click(sender As Object, e As EventArgs) Handles btnBorrar.Click
+        If registroSel <> -1 Then
+            dgPersonas.Rows.RemoveAt(registroSel)
+            lblDatos.Text = ""
         End If
     End Sub
 End Class
